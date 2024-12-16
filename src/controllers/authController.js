dc7d0b2ac5376ee9ef221e467b4657946fabf91a
gambaesi <yngeun22@gmail.com>
2024-12-17 01:07:14 +0900

    fix: 오타 수정

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,7 +4,7 @@ exports.signup = async (req, res, next) => {
     try{
         const { email, password, name, ...optionalData } = req.body;
         if (!email || !password || !name) {
-            return res.error('필수 입력값이 누락되었습니다.', 'MISSING_REQUIRED_FIELDS", 400');
+            return res.error('필수 입력값이 누락되었습니다.', 'MISSING_REQUIRED_FIELDS', 400);
         }
 
         const result = await authService.createUser({ email, password, name, ...optionalData });
@@ -24,14 +24,14 @@ exports.login = async (req, res, next) => {
     try {
         const { email, password } = req.body;
         if (!email || !password) {
-            return res.error("이메일과 비밀번호를 입력해주세요.", "MISSING_CREDENTIALS", 400);
+            return res.error('이메일과 비밀번호를 입력해주세요.', 'MISSING_CREDENTIALS', 400);
         }
 
         const result = await authService.login({ email, password });
 
         // 이메일이나 비밀번호가 틀린 경우
         if (result.error) {
-            return res.error(result.error, "INVALID_CREDENTIALS", 400);
+            return res.error(result.error, 'INVALID_CREDENTIALS', 400);
         }
 
         return res.success('로그인에 성공했습니다.', { user: result }, 200);
