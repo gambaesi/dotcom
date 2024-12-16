66dc235737ba4905174a1245150123dde8f2291e
gambaesi <yngeun22@gmail.com>
2024-12-16 16:07:57 +0900

    refactor: 사용되지 않는 utils/transform.js 파일 삭제

--- src/utils/transform.js
+++
@@ -1,9 +0,0 @@
-const camelToSnake = (str) => {
-    return str.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
-};
-
-const snakeToCamel = (str) => {
-    return str.replace(/_([a-z])/g, (match, letter) => letter.toUpperCase());
-};
-
-module.exports = { camelToSnake, snakeToCamel };
