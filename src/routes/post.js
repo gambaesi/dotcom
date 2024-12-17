ff43ed8e6aaf93d199cbf2b3665b1f99740dbe86
gambaesi <yngeun22@gmail.com>
2024-12-17 17:15:16 +0900

    feat(auth): 인증 미들웨어 추가

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const authenticateToken = require('../middleware/authMiddleware');
 const { createPost, updatePostById, softDeletePostById, getPostById, getPosts } = require('../controllers/postController');
+
+// 인증 미들웨어
+router.use(authenticateToken);
 
 // 게시글 생성, 수정, 삭제, 조회, 목록 조회
 router.post('/', createPost);
