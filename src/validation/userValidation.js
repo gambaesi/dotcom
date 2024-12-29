18352d3c2518886ea9db0199f611f6c0acdb5a0e
gambaesi <yngeun22@gmail.com>
2024-12-29 15:59:58 +0900

    feat: 회원가입 선택 필드 validation 커스텀 메세지 추가

--- src/validation/userValidation.js
+++ src/validation/userValidation.js
@@ -2,32 +2,39 @@ const Joi = require('joi');
 
 const signUpSchema = Joi.object({
     email: Joi.string().email().required().messages({
-        'string.email': '유효한 이메일 형식이 아닙니다.',
-        'any.required': '이메일은 필수 항목입니다.'
+        "string.email": "유효한 이메일 형식이 아닙니다.",
+        "any.required": "이메일은 필수 항목입니다."
     }),
     password: Joi.string().min(2).required().messages({
-        'string.min': '비밀번호는 최소 2자 이상이어야 합니다.',
-        'any.required': '비밀번호는 필수 항목입니다.'
+        "string.min": "비밀번호는 최소 2자 이상이어야 합니다.",
+        "any.required": "비밀번호는 필수 항목입니다."
     }),
     name: Joi.string().min(2).required().messages({
-        'string.min': '이름은 최소 2자 이상이어야 합니다.',
-        'any.required': '이름은 필수 항목입니다.'
+        "string.min": "이름은 최소 2자 이상이어야 합니다.",
+        "any.required": "이름은 필수 항목입니다."
     }),
-    gender: Joi.string().valid('M', 'F', 'O').optional(),
-    birthDate: Joi.date().iso().optional(), // YYYY-MM-DD
-    phoneNumber: Joi.string()
-})
+    gender: Joi.string().valid('M', 'F', 'O').optional().messages({
+        "any.only": "성별은 'M', 'F', 'O' 중 하나이어야 합니다."
+    }),
+    birthDate: Joi.date().iso().optional().messages({
+        "date.format": "생년월일은 'YYYY-MM-DD' 형식으로 입력해주세요.",
+        "date.base": "유효한 날짜를 입력해주세요.",
+    }),
+    phoneNumber: Joi.string().pattern(/^\d{3}-\d{4}-\d{4}$/).optional().messages({
+        "string.pattern.base": "전화번호는 '000-0000-0000' 형식으로 입력해주세요."
+    }),
+});
 
 const loginSchema = Joi.object({
     email: Joi.string().email().required().messages({
-        'string.email': '유효한 이메일 형식이 아닙니다.',
-        'any.required': '이메일을 입력해주세요.',
-        'string.empty': '이메일을 입력해주세요.',
+        "string.email": "유효한 이메일 형식이 아닙니다.",
+        "string.empty": "이메일을 입력해주세요.",
+        "any.required": "이메일을 입력해주세요."
     }),
     password: Joi.string().required().messages({
-        'any.required': '비밀번호를 입력해주세요.',
-        'string.empty': '비밀번호를 입력해주세요.',
-    })
+        "string.empty": "비밀번호를 입력해주세요.",
+        "any.required": "비밀번호를 입력해주세요."
+    }),
 });
 
 
