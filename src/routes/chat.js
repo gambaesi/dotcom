94412aff11e974b37ed1154eab7326bdadf81327
gambaesi <yngeun22@gmail.com>
2025-01-15 15:52:19 +0900

    feat: 기본 인덱스 라우터 화면 추가

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -2,8 +2,12 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 
-router.get("/chat", (req, res) => {
+router.get('/chat', (req, res) => {
     res.sendFile(path.join(__dirname, '..', '..', 'views', 'chat.html'));
+});
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', '..', 'views', 'index.html'));
 });
 
 module.exports = router;
